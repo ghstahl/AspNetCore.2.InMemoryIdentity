7a84d233e0210c88e9da320fcb74c71c7a960d12
Herb Stahl <ghstahl@gmail.com>
2017-10-13 16:02:02 -0700

    Initial, doesn't work

---
+++ src/WebApplication1/InMemory/InMemoryIdentityServiceCollectionExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace WebApplication1.InMemory
+{
+    public static class InMemoryIdentityServiceCollectionExtensions
+    {
+        public static IdentityBuilder AddIdentity<TUser>(this IServiceCollection services, IConfiguration configuration)
+            where TUser : class => services.AddIdentity<TUser>(configuration,null);
+
+        public static IdentityBuilder AddIdentity<TUser>(this IServiceCollection services, IConfiguration configuration,Action<IdentityOptions> setupAction)
+            where TUser : class
+        {
+            // Services used by identity
+            var authenticationBuilder = services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+                    options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+                })
+                .AddCookie(IdentityConstants.ApplicationScheme, o =>
+                {
+                    o.LoginPath = new PathString("/Account/Login");
+                    o.Events = new CookieAuthenticationEvents
+                    {
+                        OnValidatePrincipal = SecurityStampValidator.ValidatePrincipalAsync
+                    };
+                })
+                .AddCookie(IdentityConstants.ExternalScheme, o =>
+                {
+                    o.Cookie.Name = IdentityConstants.ExternalScheme;
+                    o.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                })
+                .AddCookie(IdentityConstants.TwoFactorRememberMeScheme,
+                    o => o.Cookie.Name = IdentityConstants.TwoFactorRememberMeScheme)
+                .AddCookie(IdentityConstants.TwoFactorUserIdScheme, o =>
+                {
+                    o.Cookie.Name = IdentityConstants.TwoFactorUserIdScheme;
+                    o.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                });
+            if (!(string.IsNullOrEmpty(configuration["Google-ClientId"]) ||
+                  string.IsNullOrEmpty(configuration["Google-ClientSecret"])))
+            {
+                authenticationBuilder.AddOpenIdConnect(GoogleDefaults.AuthenticationScheme, GoogleDefaults.DisplayName,
+                    o =>
+                    {
+                        var googleOpenIdConnectOptions = new GoogleOpenIdConnectOptions();
+                        o.CallbackPath = googleOpenIdConnectOptions.CallbackPath;
+
+                        o.ClientId = configuration["Google-ClientId"];
+                        o.ClientSecret = configuration["Google-ClientSecret"];
+
+                        o.Authority = googleOpenIdConnectOptions.Authority;
+                        o.ResponseType = googleOpenIdConnectOptions.ResponseType;
+                        o.GetClaimsFromUserInfoEndpoint = googleOpenIdConnectOptions.GetClaimsFromUserInfoEndpoint;
+                        o.SaveTokens = googleOpenIdConnectOptions.SaveTokens;
+
+                        o.Events = new OpenIdConnectEvents()
+                        {
+                            OnRedirectToIdentityProvider = (context) =>
+                            {
+                                if (context.Request.Path != "/Account/ExternalLogin"
+                                    && context.Request.Path != "/Manage/LinkLogin")
+                                {
+                                    context.Response.Redirect("/account/login");
+                                    context.HandleResponse();
+                                }
+
+                                return Task.FromResult(0);
+                            }
+                        };
+
+                    });
+            }
+            // Hosting doesn't add IHttpContextAccessor by default
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // Identity services
+            services.TryAddScoped<IUserValidator<TUser>, UserValidator<TUser>>();
+            services.TryAddScoped<IPasswordValidator<TUser>, PasswordValidator<TUser>>();
+            services.TryAddScoped<IPasswordHasher<TUser>, PasswordHasher<TUser>>();
+            services.TryAddScoped<ILookupNormalizer, UpperInvariantLookupNormalizer>();
+
+            // No interface for the error describer so we can add errors without rev'ing the interface
+            services.TryAddScoped<IdentityErrorDescriber>();
+            services.TryAddScoped<ISecurityStampValidator, SecurityStampValidator<TUser>>();
+            services.TryAddScoped<IUserClaimsPrincipalFactory<TUser>, UserClaimsPrincipalFactory<TUser>>();
+            services.TryAddScoped<UserManager<TUser>, AspNetUserManager<TUser>>();
+            services.TryAddScoped<SignInManager<TUser>, SignInManager<TUser>>();
+
+            if (setupAction != null)
+            {
+                services.Configure(setupAction);
+            }
+
+            return new IdentityBuilder(typeof(TUser), services);
+        }
+    }
+}
---
+++ src/WebApplication1/InMemory/InMemoryUserStore.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.InMemory
+{
+    public class InMemoryUserStore<TUser> : 
+        IUserStore<TUser>,
+        IUserPasswordStore<TUser>,
+        IUserClaimStore<TUser>,
+        IUserLoginStore<TUser> where TUser:IdentityUser
+    {
+        private Dictionary<string, TUser> userMap = new Dictionary<string, TUser>();
+        private IList<TUser> userList = new List<TUser>();
+        public Task<IdentityResult> CreateAsync(TUser user, CancellationToken cancellationToken)
+        {
+            userMap[user.Id] = user;
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public Task<IdentityResult> DeleteAsync(TUser user, CancellationToken cancellationToken)
+        {
+            userMap.Remove(user.Id);
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public void Dispose()
+        {     
+        }
+
+        public Task<TUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            TUser user;
+            if (userMap.TryGetValue(userId, out user))
+            {
+                return Task.FromResult<TUser>(user);
+            }
+            return Task.FromResult<TUser>(null);
+        }
+
+        public Task<TUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<TUser>(null);
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(TUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetUserIdAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<string>(user.Id);
+        }
+
+        public Task<string> GetUserNameAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<string>(user.UserName);
+        }
+
+        public async Task SetNormalizedUserNameAsync(TUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+           
+        }
+
+        public Task SetUserNameAsync(TUser user, string userName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(TUser user, CancellationToken cancellationToken)
+        {
+            userMap[user.Id] = user;
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public Task SetPasswordHashAsync(TUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult("hi");
+        }
+
+        public Task<bool> HasPasswordAsync(TUser user, CancellationToken cancellationToken)
+        {
+            var appUser = userList.FirstOrDefault(x => x.UserName.Equals(user.UserName));
+            if (appUser == null)
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+        public Task AddLoginAsync(TUser user, UserLoginInfo login, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task RemoveLoginAsync(TUser user, string loginProvider, string providerKey, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<UserLoginInfo>> GetLoginsAsync(TUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TUser> FindByLoginAsync(string loginProvider, string providerKey, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<TUser>(null);
+        }
+
+        public async Task<IList<Claim>> GetClaimsAsync(TUser user, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+
+        public Task AddClaimsAsync(TUser user, IEnumerable<Claim> claims, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReplaceClaimAsync(TUser user, Claim claim, Claim newClaim, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveClaimsAsync(TUser user, IEnumerable<Claim> claims, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<TUser>> GetUsersForClaimAsync(Claim claim, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
