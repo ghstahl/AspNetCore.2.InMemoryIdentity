a5bc7313f12ef60cf99fd98c89f720fb371d9e4e
ghstahl@gmail.com <ghstahl@gmail.com>
2017-10-15 20:37:30 -0700

    Added getting access_token and id_token in about

--- src/ReferenceWebApp.ExternalIdentity/Controllers/HomeController.cs
+++ src/ReferenceWebApp.ExternalIdentity/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ReferenceWebApp.Models;
 
@@ -15,11 +17,21 @@ namespace ReferenceWebApp.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
             ViewData["Message"] = "Your application description page.";
             var result = HttpContext.User.Claims.Select(
                 c => new ClaimType { Type = c.Type, Value = c.Value });
+
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync(IdentityConstants.ExternalScheme, "access_token");
+                string idToken = await HttpContext.GetTokenAsync(IdentityConstants.ExternalScheme, "id_token");
+
+                // Now you can use them. For more info on when and how to use the 
+                // access_token and id_token, see https://auth0.com/docs/tokens
+            }
+
             return View(result);
         }
 
