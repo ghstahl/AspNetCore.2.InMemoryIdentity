403ff7bb94eb23e8668402237812b5b9baa869a8
ghstahl@gmail.com <ghstahl@gmail.com>
2017-10-16 10:14:30 -0700

    more cleanup

--- src/ReferenceWebApp.ExternalIdentity/Controllers/AccountController.cs
+++ src/ReferenceWebApp.ExternalIdentity/Controllers/AccountController.cs
@@ -125,23 +125,6 @@ namespace ReferenceWebApp.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> ConfirmEmail(string userId, string code)
-        {
-            if (userId == null || code == null)
-            {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
-            }
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                throw new ApplicationException($"Unable to load user with ID '{userId}'.");
-            }
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            return View(result.Succeeded ? "ConfirmEmail" : "Error");
-        }
-
-        [HttpGet]
         public IActionResult AccessDenied()
         {
             return View();
