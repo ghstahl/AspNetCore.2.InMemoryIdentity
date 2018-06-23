eec9966e7cb13471d02979799a94d75cbab8e2ac
ghstahl@gmail.com <ghstahl@gmail.com>
2018-06-23 12:54:01 -0700

    update

--- src/ReferenceWebApp.ExternalIdentity/Models/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using Microsoft.AspNetCore.Identity;
-
-namespace ReferenceWebApp.Models
-{
-    public class ApplicationRole : MemoryRole
-    {
-    }
-}
--- src/ReferenceWebApp.ExternalIdentity/Models/ClaimType.cs
+++
@@ -1,8 +0,0 @@
-namespace ReferenceWebApp.Models
-{
-    public class ClaimType
-    {
-        public string Type { get; set; }
-        public string Value { get; set; }
-    }
-}
